6cd37b1ae6c00030e0aec3ee7b80d81ca780e306
brokamal <kamalbc@keemail.me>
2025-08-14 14:06:56 +0700

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 
+
+
+
 export default function Home() {
   return (
     <main>
-      <section style={{ textAlign: "left", margin:"auto", paddingLeft: "350px", }}>
+     <section style={{ textAlign: "left", margin:"auto", paddingLeft: "350px", }}>
         <h1 style={{ color: "#FF6969", fontSize: "56px", marginBottom: "16px" }}>About Me</h1>
         <p style={{ maxWidth: "600px", lineHeight: 1.5 }}>
           My name is Mohammad Kamal , I am a software engineer from Indonesia with experience in mobile development, web development, computer vision, and natural language processing.
@@ -11,6 +14,12 @@ export default function Home() {
         <p>Previously, I worked as a Mobile Developer in <a href="https://www.linkedin.com/company/dptsi-its/" className="hyperlink">DPTSI</a> . </p>
         <p>Check out some of my <Link href="/portfolio" className="hyperlink">Projects</Link> </p>
       </section>
+      <p> </p>
+      <div className="contains pl-[270px]">
+      <div className="flex">
+            <Link href="https://github.com" className="text-xl underline">Github</Link>
+      </div>
+      </div>
     </main>
   );
 }
