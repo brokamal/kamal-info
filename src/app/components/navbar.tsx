a1da450c6abb78bcb521d18da3f9af6839d2f9cf
brokamal <kamalbc@keemail.me>
2025-08-16 08:30:51 +0700

    edit

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -5,7 +5,6 @@ const Navbar: FC = () => {
   return (
 
 <nav className="py-3 bg-[#3c3836] text-[#F9D094] w-full">
-  {/* Left Links */}
   <div className="max-w-xl mx-4 lg:mx-auto flex md:flex-row gap-4 md:gap-6 mb-2 md:mb-0">
     <Link
       href="/"
