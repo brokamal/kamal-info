358778a2e0f04cfc84ec0e4f81c00cbac77d1033
brokamal <kamalbc@keemail.me>
2025-08-18 10:40:18 +0700

    edit typeface/font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
       <section>
        <h1 className="text-[#F9D094] text-5xl mb-4 font-helvetica font-bold tracking-tight">About Me</h1>
-        <p className="mb-4 font-serif text-white">
+        <p className="mb-4 font-sans text-white">
           My name is Mohammad Kamal, I am a software engineer from Indonesia with experience in mobile development, web development, computer vision, and natural language processing.
         </p>
-        <p className="font-helvetica font-bold text-white">
+        <p className="font-sans text-white">
           Previously, I worked as a Mobile Developer at{" "}
           <a
             href="https://www.linkedin.com/company/dptsi-its/"
@@ -17,25 +17,25 @@ export default function Home() {
           </a>
           
         </p>
-        <p className="font-bold font-helvetica text-white">
+        <p className="font-sans text-white">
           Check out some of my{" "}
           <Link href="/projects" className="text-[#FF6969] hover:underline">
             Projects
           </Link>
         </p>
-      <p className="font-helvetica text-white">
+      <p className="font-sans text-white">
         Stacks: C, C++, Python, Java, Kotlin, Javascript, gold.
       </p>
       <div className="flex font-helvetica gap-12 justify-center text-amber-500 sm:justify-start max-w-3xl mt-6">
         <a
           href="https://github.com/brokamal"
-          className="flex items-center gap-1 font-helvetica font-bold hover:text-amber-100 hover:underline"
+          className="flex items-center gap-1 font-sans font-bold hover:text-amber-100 hover:underline"
         >
             ↖︎ Github 
         </a>
         <a
           href="https://linkedin.com/in/mohammad-kamal-bdg"
-          className="flex items-center gap-1 text-amber-500 font-helvetica font-bold hover:text-amber-100 hover:underline"
+          className="flex items-center gap-1 text-amber-500 font-sans font-bold hover:text-amber-100 hover:underline"
         >
            ↖︎ Linkedin
         </a>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function ProjectsPage() {
   return (
     <main>
       <h1 className="text-[#F9D094] text-5xl mb-4 font-helvetica font-bold tracking-tight">Projects</h1>
-        <p className="mb-4 font-helvetica font-bold text-white">
+        <p className="mb-4 font-sans text-white">
           Here are some of my projects that I have worked on.
         </p>
    </main>
