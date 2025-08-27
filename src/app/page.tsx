8d053d1189df9c2ae6a05fbd088c370d4526ef2e
brokamal <kamalbc@keemail.me>
2025-08-27 11:31:59 +0700

    edit project page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-// const section ok
 
 export default function Home() {
   return (
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,19 +1,42 @@
 import "../globals.css";
 import Link from "next/link"
 
+const projectItems = {
+  'https://github.com/brokamal/SundaScriptOCR':{
+    name: 'Sundanese Script OCR',
+    },
+  'https://github.com/brokamal/CuacaApp':{
+    name: 'CuacaApp',
+    },
+  'https://github.com/brokamal/nolimit-ds-test-mohammad-kamal':{
+    name: 'Rotten Tomatoes Review Sentiment Classifier',
+    },
+  }
+
 
 export default function ProjectsPage() {
   return (
-    <main>
-      <h1 className="text-[#F9D094] text-5xl mb-4 font-helvetica font-bold tracking-tight">Projects</h1>
-        <p className="mb-4 font-sans text-white">
-          Here are some of my projects that I have worked on.
-        </p>
-      <Link href="https://github.com/brokamal/SundaScriptOCR" className="text-amber-500 hover:text-white underline text-xl">Sundanese Script OCR</Link> 
-      <p></p>
-      <Link href="https://github.com/brokamal/CuacaApp" className="text-amber-500 hover:text-white underline text-xl">CuacaApp</Link> 
-      <p></p>
-      <Link href="https://github.com/brokamal/nolimit-ds-test-mohammad-kamal" className="text-amber-500 hover:text-white underline text-xl">Rotten Tomatoes Review Sentiment Classifier</Link>
-   </main>
+    <>
+      <h1 className="text-[#F9D094] text-5xl mb-4 font-helvetica font-bold tracking-tight">
+        Projects
+      </h1>
+      <p className="mb-4 font-sans text-white">
+        Here are some of my projects that I have worked on.
+      </p>
+      <div className="flex-col flex gap-2">
+        {Object.entries(projectItems).map(([path, { name }]) => {
+          return (
+            <Link
+              key={path}
+              href={path}
+              className="text-pink-300 hover:text-white underline text-xl"
+            >
+              {name}
+            </Link>
+          );
+        })}
+      </div>
+    </>
   );
 }
+
